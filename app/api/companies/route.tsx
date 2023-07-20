66ac013e284dd9f29a53051494fffe3b9bda77b1
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-07-20 16:29:18 -0700

    added GET POST and DELETE functions

---
+++ app/api/companies/route.tsx
@@ -0,0 +1,23 @@
+import connectDb from '@/libs/connectDb'
+import Company from '@/models/company'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET() {
+	await connectDb()
+	const companies = await Company.find()
+	return NextResponse.json({ companies })
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+	const { name, website } = await req.json()
+	await connectDb()
+	await Company.create({ name, website })
+	return NextResponse.json({ message: 'Company Added' }, { status: 201 })
+}
+
+export async function DELETE(req: NextRequest) {
+	const id = req.nextUrl.searchParams.get('id')
+	await connectDb()
+	await Company.findByIdAndDelete(id)
+	return NextResponse.json({ message: 'Company deleted' }, { status: 200 })
+}
