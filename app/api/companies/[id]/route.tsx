eca11474d3fd18ee452729143f0eed00fde9128f
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-07-20 16:29:34 -0700

    added dynamic route for PUT request

---
+++ app/api/companies/[id]/route.tsx
@@ -0,0 +1,15 @@
+import connectDb from '@/libs/connectDb'
+import Company from '@/models/company'
+import { NextResponse, NextRequest } from 'next/server'
+
+interface Params {
+	id: string
+}
+
+export async function PUT(req: NextRequest, { params }: { params: Params }) {
+	const { id } = params
+	const { newName: name, newWebsite: website } = await req.json()
+	await connectDb()
+	await Company.findByIdAndUpdate(id, { name, website })
+	return NextResponse.json({ message: 'Company updated' }, { status: 200 })
+}
