b2c0b8b0b70d0cb14b7391dc475ed291cac9b5fe
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-07-20 16:28:52 -0700

    created company model

---
+++ models/company.tsx
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from 'mongoose'
+
+interface ICompany {
+	name: string
+	website: string
+}
+
+const companySchema = new Schema<ICompany>({
+	name: { type: String, required: true },
+	website: { type: String, required: true },
+})
+
+const Company =
+	mongoose.models.Company || mongoose.model('Company', companySchema)
+
+export default Company
