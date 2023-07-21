e823371306adb0bf80bb04644dbea65b250e1173
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-07-21 16:49:48 -0700

    created removebutton component

---
+++ components/RemoveButton.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import React from 'react'
+import { RiDeleteBin6Fill } from 'react-icons/ri'
+
+interface RemoveButtonProps {
+	id: string
+}
+
+function RemoveButton({ id }: RemoveButtonProps) {
+	const handleHardRefresh = () => {
+		const url = new URL(window.location.href)
+		url.searchParams.set('refresh', 'true')
+		window.location.href = url.toString()
+	}
+
+	const deleteCompany = async () => {
+		const confirmed = confirm('Delete Company?')
+		if (confirmed) {
+			await fetch(`http://localhost:3000/api/companies?id=${id}`, {
+				method: 'DELETE',
+			})
+			handleHardRefresh()
+		}
+	}
+
+	return (
+		<button
+			onClick={deleteCompany}
+			title='delete'
+			className='text-red-600'
+		>
+			<RiDeleteBin6Fill size={24} />
+		</button>
+	)
+}
+
+export default RemoveButton
