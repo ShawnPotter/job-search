82b200e43a7f2da9bc5fb5986f91318a2a681cb4
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-07-21 16:49:03 -0700

    added dynamic get function

--- app/api/companies/[id]/route.tsx
+++ app/api/companies/[id]/route.tsx
@@ -14,3 +14,10 @@ export async function PUT(req: NextRequest, { params }: { params: Params }) {
 	await Company.findByIdAndUpdate(id, { name, website })
 	return NextResponse.json({ message: 'Company updated' }, { status: 200 })
 }
+
+export async function GET(req: NextRequest, { params }: { params: Params }) {
+	const { id } = params
+	await connectDb()
+	const company = await Company.findOne({ _id: id })
+	return NextResponse.json({ company }, { status: 200 })
+}
