b95b0183eb6cf55f42ade844b0d55fa8aee79e1c
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-07-21 16:49:28 -0700

    create form component

---
+++ components/Form.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import React, { useState } from 'react'
+
+function Form() {
+	const [name, setName] = useState('')
+	const [website, setWebsite] = useState('')
+
+	const handleHardRefresh = () => {
+		const url = new URL(window.location.href)
+		url.searchParams.set('refresh', 'true')
+		window.location.href = url.toString()
+	}
+
+	const handleSubmit = async (e: any) => {
+		e.preventDefault()
+		if (!name || !website) {
+			alert('Both fields are required')
+			return
+		}
+		try {
+			const res = await fetch('http://localhost:3000/api/companies', {
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json',
+				},
+				body: JSON.stringify({ name, website }),
+			})
+			handleHardRefresh()
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	return (
+		<div className='flex w-full mt-10 justify-center'>
+			<form
+				onSubmit={handleSubmit}
+				className='flex flex-col w-full md:w-3/4 justify-center items-center mx-10 '
+			>
+				<div className='w-full'>
+					<label>
+						Company{' '}
+						<input
+							onChange={(e) => setName(e.target.value)}
+							value={name}
+							type='text'
+							className='w-full p-1 rounded shadow-inner text-black'
+							placeholder='Company Name'
+						/>
+					</label>
+				</div>
+				<div className='w-full mt-5'>
+					<label>
+						Website{' '}
+						<input
+							onChange={(e) => setWebsite(e.target.value)}
+							value={website}
+							type='text'
+							className='w-full p-1 rounded shadow-inner text-black'
+							placeholder='Career Website'
+						/>
+					</label>
+				</div>
+				<div className='mt-5 w-full flex justify-center'>
+					<input
+						type='submit'
+						className='flex justify-center bg-blue-600 hover:bg-blue-400  py-4 w-full rounded-lg shadow align-center'
+					/>
+				</div>
+			</form>
+		</div>
+	)
+}
+
+export default Form
