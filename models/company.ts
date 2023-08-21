e05e5fd25491779eb28a0c2f2f383f5edea62c68
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-08-21 13:55:20 -0700

    updated model schema to include lastVisited field

--- models/company.ts
+++ models/company.ts
@@ -5,11 +5,13 @@ import mongoose, { Schema } from 'mongoose'
 interface ICompany {
 	name: string
 	website: string
+	lastVisited: Date
 }
 
 const companySchema = new Schema<ICompany>({
 	name: { type: String, required: true },
 	website: { type: String, required: true },
+	lastVisited: { type: Date, required: false },
 })
 
 const Company =
